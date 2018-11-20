2802d497d20438b6ad40b5f9177fc0539d9785df
Sergey Pashnin <s.pashnin@encry.ru>
2018-11-20 15:18:02 +0300

    add btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,4 +42,6 @@ export class AppComponent implements OnInit {
         this.newCarColor = '';
     }
 
+    // post
+
 }
--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -32,4 +32,6 @@ export class CarsService {
             }));
     }
 
+    // post
+
 }
